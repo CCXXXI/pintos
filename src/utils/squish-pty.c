3a0164e4bb545ae1e7f5b978f660cae0ce3a4ec0
CCXXXI <ccxxxi@qq.com>
2020-10-26 22:58:11 +0800

    fix <stropts.h>
    竟然使用这么过时的库，还得手动注释掉

--- src/utils/squish-pty.c
+++ src/utils/squish-pty.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stropts.h>
+// #include <stropts.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <sys/time.h>
@@ -285,12 +285,12 @@ main (int argc __attribute__ ((unused)), char *argv[])
 
   /* System V implementations need STREAMS configuration for the
      slave. */
-  if (isastream (slave))
-    {
-      if (ioctl (slave, I_PUSH, "ptem") < 0
-          || ioctl (slave, I_PUSH, "ldterm") < 0)
-        fail_io ("ioctl");
-    }
+  // if (isastream (slave))
+  //   {
+  //     if (ioctl (slave, I_PUSH, "ptem") < 0
+  //         || ioctl (slave, I_PUSH, "ldterm") < 0)
+  //       fail_io ("ioctl");
+  //   }
 
   /* Arrange to get notified when a child dies, by writing a byte
      to a pipe fd.  We really want to use pselect() and
