3185bf9e5d70d500c1e18b0aaa0ad862fb492e37
CCXXXI <ccxxxi@qq.com>
2020-11-20 14:25:10 +0800

    reformat

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -19,7 +19,7 @@ enum thread_status
 };
 
 /* Thread identifier type.
-   You can redefine this to whatever type you like. */
+    You can redefine this to whatever type you like. */
 typedef int tid_t;
 #define TID_ERROR ((tid_t)-1) /* Error value for tid_t. */
 
@@ -35,60 +35,60 @@ typedef int tid_t;
 
 /* A kernel thread or user process.
 
-   Each thread structure is stored in its own 4 kB page.  The
-   thread structure itself sits at the very bottom of the page
-   (at offset 0).  The rest of the page is reserved for the
-   thread's kernel stack, which grows downward from the top of
-   the page (at offset 4 kB).  Here's an illustration:
+    Each thread structure is stored in its own 4 kB page.  The
+    thread structure itself sits at the very bottom of the page
+    (at offset 0).  The rest of the page is reserved for the
+    thread's kernel stack, which grows downward from the top of
+    the page (at offset 4 kB).  Here's an illustration:
 
-        4 kB +---------------------------------+
-             |          kernel stack           |
-             |                |                |
-             |                |                |
-             |                V                |
-             |         grows downward          |
-             |                                 |
-             |                                 |
-             |                                 |
-             |                                 |
-             |                                 |
-             |                                 |
-             |                                 |
-             |                                 |
-             +---------------------------------+
-             |              magic              |
-             |                :                |
-             |                :                |
-             |               name              |
-             |              status             |
-        0 kB +---------------------------------+
-
-   The upshot of this is twofold:
-
-      1. First, `struct thread' must not be allowed to grow too
-         big.  If it does, then there will not be enough room for
-         the kernel stack.  Our base `struct thread' is only a
-         few bytes in size.  It probably should stay well under 1
-         kB.
-
-      2. Second, kernel stacks must not be allowed to grow too
-         large.  If a stack overflows, it will corrupt the thread
-         state.  Thus, kernel functions should not allocate large
-         structures or arrays as non-static local variables.  Use
-         dynamic allocation with malloc() or palloc_get_page()
-         instead.
-
-   The first symptom of either of these problems will probably be
-   an assertion failure in thread_current(), which checks that
-   the `magic' member of the running thread's `struct thread' is
-   set to THREAD_MAGIC.  Stack overflow will normally change this
-   value, triggering the assertion. */
+         4 kB +---------------------------------+
+              |          kernel stack           |
+              |                |                |
+              |                |                |
+              |                V                |
+              |         grows downward          |
+              |                                 |
+              |                                 |
+              |                                 |
+              |                                 |
+              |                                 |
+              |                                 |
+              |                                 |
+              |                                 |
+              +---------------------------------+
+              |              magic              |
+              |                :                |
+              |                :                |
+              |               name              |
+              |              status             |
+         0 kB +---------------------------------+
+ 
+    The upshot of this is twofold:
+ 
+       1. First, `struct thread' must not be allowed to grow too
+          big.  If it does, then there will not be enough room for
+          the kernel stack.  Our base `struct thread' is only a
+          few bytes in size.  It probably should stay well under 1
+          kB.
+ 
+       2. Second, kernel stacks must not be allowed to grow too
+          large.  If a stack overflows, it will corrupt the thread
+          state.  Thus, kernel functions should not allocate large
+          structures or arrays as non-static local variables.  Use
+          dynamic allocation with malloc() or palloc_get_page()
+          instead.
+ 
+    The first symptom of either of these problems will probably be
+    an assertion failure in thread_current(), which checks that
+    the `magic' member of the running thread's `struct thread' is
+    set to THREAD_MAGIC.  Stack overflow will normally change this
+    value, triggering the assertion. */
 /* The `elem' member has a dual purpose.  It can be an element in
-   the run queue (thread.c), or it can be an element in a
-   semaphore wait list (synch.c).  It can be used these two ways
-   only because they are mutually exclusive: only a thread in the
-   ready state is on the run queue, whereas only a thread in the
-   blocked state is on a semaphore wait list. */
+    the run queue (thread.c), or it can be an element in a
+    semaphore wait list (synch.c).  It can be used these two ways
+    only because they are mutually exclusive: only a thread in the
+    ready state is on the run queue, whereas only a thread in the
+    blocked state is on a semaphore wait list. */
 struct thread
 {
     /* Owned by thread.c. */
@@ -119,8 +119,8 @@ struct thread
 };
 
 /* If false (default), use round-robin scheduler.
-   If true, use multi-level feedback queue scheduler.
-   Controlled by kernel command-line option "-o mlfqs". */
+    If true, use multi-level feedback queue scheduler.
+    Controlled by kernel command-line option "-o mlfqs". */
 extern bool thread_mlfqs;
 
 void thread_init(void);
