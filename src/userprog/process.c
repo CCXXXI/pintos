890a40b1e0b0a20d400a63567b8034ee7f90f1bf
CCXXXI <ccxxxi@qq.com>
2020-12-29 11:39:10 +0800

    fix typo

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -22,7 +22,7 @@ static thread_func start_process NO_RETURN;
 static bool load(const char *cmdline, void (**eip)(void), void **esp);
 
 /* Starts a new thread running a user program loaded from
-   FILENAME.  The new thread may be scheduled (and may even exit)
+   FILE_NAME.  The new thread may be scheduled (and may even exit)
    before process_execute() returns.  Returns the new process's
    thread id, or TID_ERROR if the thread cannot be created. */
 tid_t process_execute(const char *file_name)
