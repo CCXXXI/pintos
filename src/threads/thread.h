a1299d8caba959df1374f2b49e4d9fe125107d6b
CCXXXI <ccxxxi@qq.com>
2021-01-10 01:20:40 +0800

    exit code

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -122,6 +122,7 @@ struct thread
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir; /* Page directory. */
+    int exit_code;     /* Exit code. */
 #endif
 
     /* Owned by thread.c. */
