17e3ff6d9dcc90cf4c5b7a97460f59f8bfd152b2
CCXXXI <ccxxxi@qq.com>
2021-01-09 21:25:11 +0800

    user memory access

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,8 +3,11 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "userprog/pagedir.h"
 
 static void syscall_handler(struct intr_frame *);
+static bool is_valid_ptr(const void *);
 
 void syscall_init(void)
 {
@@ -15,4 +18,12 @@ static void syscall_handler(struct intr_frame *f UNUSED)
 {
     printf("system call!\n");
     thread_exit();
+}
+
+/* Check if the user did not pass a null pointer,
+    a pointer to kernel virtual address space
+    or a pointer to unmapped virtual memory. */
+static bool is_valid_ptr(const void *ptr)
+{
+    return ptr != NULL && is_user_vaddr(ptr) && pagedir_get_page(thread_current()->pagedir, ptr) != NULL;
 }
